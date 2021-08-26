97b09d23a2d659de8eb56adc1dadbf93b5e72f01
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2021-08-26 15:40:30 -0500

    change template

--- WebPage/Models/BlogIndexModel.cs
+++ WebPage/Models/BlogIndexModel.cs
@@ -5,10 +5,7 @@ namespace Personal.Models
 {
     public class BlogIndexModel
     {
-        public BlogIndexModel()
-        {
-        }
-
+        public BlogIndexModel() { }
         public List<Post> Entradas { get; set; }
     }
 }
