12f7fd80d313f9ad86eb003fe668a4ec71d1380a
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2021-08-28 13:59:40 -0500

    lifetimes

--- WebPage/Startup.cs
+++ WebPage/Startup.cs
@@ -21,9 +21,8 @@ namespace Personal
         {
             KeyVault.SpeechKey = Configuration["speechKey1"];
             services.AddControllersWithViews(ConfigureMvcOptions);         
-            services.AddEntityFrameworkSqlite().AddDbContext<SQLiteContext>();
-            services.AddTransient<ITransientService, ServicioEjemplo>();
-            services.AddScoped<IScopedService, ServicioEjemplo>();
+            services.AddEntityFrameworkSqlite().AddDbContext<SQLiteContext>();            
+            services.AddScoped<IScopedService, ServicioEjemplo>();            
             services.AddSingleton<ISingletonService, ServicioEjemplo>();
         }
 
