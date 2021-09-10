8161bf44f3386e3a007ce31a71a374151f6453ef
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2021-09-10 16:22:13 -0500

    Extract SQLite Example

--- WebPage/Controllers/Work/WorkController.cs
+++ WebPage/Controllers/Work/WorkController.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Personal.Data;
-using Personal.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 namespace Personal.Controllers.Work
 {
     public partial class WorkController : Controller
     {
         readonly IScopedService _scopedService;
         readonly ISingletonService _singletonService;
-
         public WorkController(IScopedService scoped, ISingletonService singleton)
         {
             _scopedService = scoped;
@@ -21,49 +15,6 @@ namespace Personal.Controllers.Work
         {
             ViewBag.Titulo = "Portafolio";
             return View();
-        }
-        private readonly SQLiteContext _myDbContext = new();
-        public ActionResult SQLite()
-        {
-            var model = new SQLiteModel
-            {
-                Visitantes = _myDbContext.Visitantes.OrderByDescending(v => v.Fecha).Take(50).ToList()
-            };
-            return View(model);
-        }
-        public async Task<JsonResult> SQLiteAgregarVisitante(string id)
-        {
-            if (id != null)
-            {
-                if (id.Length > 50) //chop chop chop      
-                    id = id.Substring(0, 50);
-
-                var respuesta = new ResultObject { OK = false, Name = id };
-
-                try
-                {
-                    var visita = new Visitante
-                    {
-                        Nombre = id,
-                        Fecha = DateTime.Now,
-                        Id = respuesta.GUID
-                    };
-                    _myDbContext.Visitantes.Add(visita);
-                    await _myDbContext.SaveChangesAsync();
-                    respuesta.OK = true;
-                    respuesta.Attributes.Add("Fecha", visita.Fecha.ToString());
-                }
-                catch
-                {
-                    respuesta.Name = "Error al guardar en la base de datos";
-                }
-                return Json(respuesta);
-            }
-            else
-            {
-                var respuesta = new ResultObject { OK = false, Name = "No ha especificador un nombre" };
-                return Json(respuesta);
-            }
-        }
+        }       
     }
 }
