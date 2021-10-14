a636927b1a84d5ebf8f02b2e1aad7f816b15ad73
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2021-10-14 15:28:53 -0500

    Update SQLiteContext.cs

--- WebPage/Data/SQLiteContext.cs
+++ WebPage/Data/SQLiteContext.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-
 namespace Personal.Data
 {
     public class SQLiteContext : DbContext
     {
         public DbSet<Visitante> Visitantes { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = @"wwwroot\data\SQLite.db" };
             var connectionString = connectionStringBuilder.ToString();
             var connection = new SqliteConnection(connectionString);
-
             optionsBuilder.UseSqlite(connection);
         }
     }
