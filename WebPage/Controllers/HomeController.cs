d5c801d914d9cfc96961b8926e7afa59b41d13ae
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2021-11-04 14:04:24 -0600

    add multilanguage support

--- WebPage/Controllers/HomeController.cs
+++ WebPage/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace Personal.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+        }
+        private readonly IStringLocalizer<HomeController> _localizer;
         public ActionResult Index()
         {
-            ViewBag.Titulo = "Inicio";           
-            return View();           
+            ViewBag.Titulo = _localizer["Title"];
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
--- WebPage/Startup.cs
+++ WebPage/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
 using Personal.Data;
+using System.Globalization;
 namespace Personal
 {
     public class Startup
@@ -19,41 +23,47 @@ namespace Personal
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<SecurityContext>();
-            services.AddRazorPages();            
-            services.AddControllersWithViews(ConfigureMvcOptions);            
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddRazorPages().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();            
+            services.AddControllersWithViews(ConfigureMvcOptions);
             services.AddScoped<IScopedService, ExampleService>();
             services.AddSingleton<ISingletonService, ExampleService>();
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("es-MX"),
+                    new CultureInfo("fr-fr")
+                };
+                options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+            if (env.IsDevelopment())            
                 app.UseDeveloperExceptionPage();
-            }
             else
             {
                 app.UseHsts();
             }
-
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseAuthorization();            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                name: "areas",
-                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}/{offset?}"
-                );
-
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(name: "areas", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}/{offset?}");
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
         private void ConfigureMvcOptions(MvcOptions mvcOptions)
-        {
+        {            
         }
     }
 }
