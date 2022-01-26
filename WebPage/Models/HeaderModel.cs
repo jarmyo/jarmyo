7f23ffcfa124e8b8585b23e06d050d5cc17a7165
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-01-26 11:22:13 -0600

    Convert navigation bar to partial ASP.NET translated component

---
+++ WebPage/Models/HeaderModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+
+namespace Personal.Models
+{
+    public class HeaderModel
+    {
+        public LocalizedHtmlString Title { get; set; }
+        public string Active
+        {
+            set
+            {
+                switch (value)
+                {
+                    case "CV":
+                        {
+                            ActiveCV = "active";
+                            break;
+                        }
+                    case "Work":
+                        {
+                            ActiveWork = "active";
+                            break;
+                        }
+                    case "Blog":
+                        {
+                            ActiveBlog = "active";
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+        public string ActiveCV { get; set; } = string.Empty;
+        public string ActiveWork { get; set; } = string.Empty;
+        public string ActiveBlog { get; set; } = string.Empty;
+    }
+}
