e98d2007f121d4102045c65108ae25d7ca932845
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-05-10 14:03:49 -0500

    automated testing

--- Tests/Controllers/Work/WorkControllerTests.cs
+++ Tests/Controllers/Work/WorkControllerTests.cs
@@ -1,25 +1,57 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Personal.Controllers.Work;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Personal.Controllers.Work.Tests
 {
     [TestClass()]
     public class WorkControllerTests
     {
+        private static Dictionary<short, string> ResultStack = new Dictionary<short, string>()
+        {
+            [1] = "1.00000",
+            [2] = "1.41421",
+            [3] = "1.73205",
+            [4] = "2.00000",
+            [5] = "2.23607",
+            [6] = "2.44949",
+            [7] = "2.64575",
+            [8] = "2.82843",
+            [9] = "3.00000",
+            [10] = "3.16228",
+            [11] = "3.31662",
+            [12] = "3.46410",
+            [13] = "3.60555",
+            [14] = "3.74166",
+            [15] = "3.87298",
+            [16] = "4.00000",
+            [17] = "4.12311",
+            [18] = "4.24264",
+            [19] = "4.35890"
+        };
+        private static List<string> ValidShort = new List<string>()
+        {
+            "0",
+            "12345678901",
+            "-12345678901"
+        };
+        private static List<string> NotValidShort = new List<string>()
+        {
+             "1239223372036854775809",
+            "-1239223372036854775809",
+            "",
+            "abc",
+            "1.1.1",
+            "1.1"
+        };
         [TestMethod()]
         public void IsNotNumericTest()
         {
-            //correct values
-            Assert.IsFalse(WorkController.IsNotNumeric("0"));
-            Assert.IsFalse(WorkController.IsNotNumeric("12345678901"));
-            Assert.IsFalse(WorkController.IsNotNumeric("-12345678901"));
-            //wrong values
-            Assert.IsTrue(WorkController.IsNotNumeric("1239223372036854775809"));
-            Assert.IsTrue(WorkController.IsNotNumeric("-1239223372036854775809"));
-            Assert.IsTrue(WorkController.IsNotNumeric(""));
-            Assert.IsTrue(WorkController.IsNotNumeric("abc"));
-            Assert.IsTrue(WorkController.IsNotNumeric("1.1.1"));
-            Assert.IsTrue(WorkController.IsNotNumeric("1.1"));
-        }       
+            foreach (var valid in ValidShort)
+                Assert.IsFalse(WorkController.IsNotNumeric(valid));
+            foreach (var notValid in NotValidShort)
+                Assert.IsTrue(WorkController.IsNotNumeric(notValid));
+        }
         [TestMethod()]
         public void IsNot16BitsTest()
         {
@@ -29,6 +61,24 @@ namespace Personal.Controllers.Work.Tests
             //wrong values
             Assert.AreEqual(WorkController.IsNot16Bits(short.MaxValue + 1.ToString()), true);
             Assert.AreEqual(WorkController.IsNot16Bits((short.MinValue - 1).ToString()), true);
-        }       
+        }
+        [TestMethod()]
+        public void BadResultTest()
+        {
+            var result = WorkController.BadResult("Bad response test");
+            Assert.IsFalse(result.OK);
+        }
+        [TestMethod()]
+        public void SquareResultTest()
+        {
+            foreach (var val in ResultStack)
+            {
+                var number = val.Key.ToString();
+                var square = val.Value.ToString();
+                var request = WorkController.SquareResult(number);
+                var result = $"The Square root of {number} is {square}";
+                Assert.AreEqual(request.Name, result);
+            }
+        }
     }
 }
