110343a75618799f4dfc2b749a2484231b9e64f9
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-05-10 13:09:45 -0500

    new script

--- WebPage/Assets/TypeScript/home.ts
+++ WebPage/Assets/TypeScript/home.ts
@@ -1,1 +1,8 @@
 ﻿// Pues tambien le muevo al typescript.
+function toggleTheme(theme: string): void {
+    var refStart: string = "https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/";
+    var refEnd: string = "/bootstrap.min.css";
+    var styleSheet = <HTMLLinkElement>document.getElementById('themeSheet');
+    styleSheet.href = refStart + theme + refEnd;
+    console.log(styleSheet.href);
+}
