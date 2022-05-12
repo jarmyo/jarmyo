f0bc3e8fcef1cfb4602aae176793e75739ae693f
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-05-12 14:48:37 -0500

    serialization support

--- WebPage/Controllers/Work/ResultObject.cs
+++ WebPage/Controllers/Work/ResultObject.cs
@@ -1,4 +1,6 @@
-﻿namespace Personal.Controllers
+﻿using System.Xml;
+using System.Xml.Serialization;
+namespace Personal.Controllers
 {
     public class ResultObject : IDisposable
     {
@@ -8,14 +10,32 @@
             Attributes = new Dictionary<string, string>();
         }
         public bool OK { get; set; }
-        public string GUID { get; }
+        public string GUID { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
+        [XmlIgnore]
         public Dictionary<string, string> Attributes { get; set; }
         public void Dispose()
         {
             GC.SuppressFinalize(this);
             // clean all?
+        }
+        public string SerializeXml()
+        {
+            XmlSerializer xmlSerializer = new(typeof(ResultObject));
+            XmlSerializer xsSubmit = xmlSerializer;
+            string xml = string.Empty;
+            XmlWriterSettings settings = new()
+            {
+                OmitXmlDeclaration = false
+            };
+            using (var sww = new StringWriter())
+            using (XmlWriter writer = XmlWriter.Create(sww, settings))
+            {
+                xsSubmit.Serialize(writer, this);
+                xml = sww.ToString();
+            }
+            return xml;
         }
     }
 }
