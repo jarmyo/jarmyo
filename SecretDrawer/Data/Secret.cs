def89c6c2a0f81a2809137df109d3daf0e8576b2
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-06-15 09:54:23 -0500

    Data structures first definition

---
+++ SecretDrawer/Data/Secret.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretDrawer.Data
+{
+    public class Secret
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Hash { get; set; }
+        public string? Color { get; set; }
+        public int Order { get; set; }
+        public int IdCategory { get; set; }        
+    }
+}
