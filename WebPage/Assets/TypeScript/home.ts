63dbb190266ed34acfa7c052b4584dc39b57ae4b
codefactor-io <support@codefactor.io>
2022-06-15 16:28:47 +0000

    [CodeFactor] Apply fixes

--- WebPage/Assets/TypeScript/home.ts
+++ WebPage/Assets/TypeScript/home.ts
@@ -1,8 +1,8 @@
 ﻿// Pues tambien le muevo al typescript.
 function toggleTheme(theme: string): void {
-    var refStart: string = "https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/";
-    var refEnd: string = "/bootstrap.min.css";
-    var styleSheet = <HTMLLinkElement>document.getElementById('themeSheet');
+    let refStart: string = "https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/";
+    let refEnd: string = "/bootstrap.min.css";
+    let styleSheet = <HTMLLinkElement>document.getElementById('themeSheet');
     styleSheet.href = refStart + theme + refEnd;
     console.log(styleSheet.href);
 }
