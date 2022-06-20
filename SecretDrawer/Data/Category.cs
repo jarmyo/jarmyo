f04fbf38e116b2cde5e086bec1a031b13f728359
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-06-20 13:24:29 -0500

    database definition

--- SecretDrawer/Data/Category.cs
+++ SecretDrawer/Data/Category.cs
@@ -2,8 +2,9 @@
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Color { get; set; }
         public int Order { get; set; }
---
+++ SecretDrawer/Data/SecretContext.cs
@@ -0,0 +1,25 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using Microsoft.Data.Sqlite;
+global using Microsoft.EntityFrameworkCore;
+
+namespace SecretDrawer.Data
+{
+    public class SecretContext : DbContext
+    {
+        public DbSet<Secret>? Secrets{ get; set; }
+        public DbSet<Category>? Categories { get; set; }     
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = @"Data\Secrets.db" };
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+            optionsBuilder.UseSqlite(connection);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configure domain classes using modelBuilder here   
+            //modelBuilder.Entity<PostTags>()
+            //    .HasKey(o => new { o.IdPost, o.Tag });
+        }
+    }
+}
