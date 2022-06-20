0e897362166be52d5601bb5f8a847f48991db595
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-06-20 13:25:19 -0500

    initial logic for database creation

--- SecretDrawer/App.xaml.cs
+++ SecretDrawer/App.xaml.cs
@@ -1,17 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-
+using System.IO;
 namespace SecretDrawer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+           //TODO: update
+        }
+        internal static Data.SecretContext? DataContext;
+        
+        protected override async void OnStartup(StartupEventArgs e)
+        {            
+            //TODO: Check updates
+            //TODO: Check logs
+            Directory.CreateDirectory(AppContext.BaseDirectory + "\\Data");
+            DataContext = new Data.SecretContext();
+            if (!File.Exists(AppContext.BaseDirectory + @"\Data\Secrets.db"))
+            {
+                if (!await DataContext.Database.EnsureCreatedAsync())
+                {
+                    MessageBox.Show("Fail Creating Database context");
+                    return;
+                }
+            }
+
+            var w = new MainWindow();
+            w.Show();
+        }
     }
 }
