0f81ad9573e22765423f8d1996960e1f45b69fc7
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2022-06-20 13:22:44 -0500

    nullable var

--- SecretDrawer/Data/Secret.cs
+++ SecretDrawer/Data/Secret.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,13 @@ namespace SecretDrawer.Data
 {
     public class Secret
     {
+        [Key]
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
         public string? Hash { get; set; }
         public string? Color { get; set; }
         public int Order { get; set; }
-        public int IdCategory { get; set; }        
+        public int? IdCategory { get; set; }        
     }
 }
